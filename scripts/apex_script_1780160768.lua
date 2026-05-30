a2b90cb86d66c9be62e2736af06af4977f3c64b8
Black Academy <blackacademyprivate@gmail.com>
2026-05-30 17:23:14 -0300

    APEX: limpando scripts antigos

--- scripts/apex_script_1780160768.lua
+++
@@ -1,5 +0,0 @@
-local TextChatService = game:GetService("TextChatService")
-local textChannel = TextChatService.TextChannels.RBXGeneral -- Canal padrão
-
--- Isso envia uma mensagem como se fosse o jogador
-textChannel:SendAsync("Olá, estou usando o Delta!")
